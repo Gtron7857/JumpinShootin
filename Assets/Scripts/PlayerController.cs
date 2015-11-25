cd003aa1e3a114ace890f927387460ff68ba5dfe
Wade Shepardson <shepardson.wade@gmail.com>
2015-11-24 22:23:17 -0600

    slidy cubey

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+  private float moveForce = 100f;
+  private float maxSpeed = 5f;
+
+  // Use this for initialization
+  void Start() {
+  
+  }
+  
+  // Update is called once per frame
+  void Update() {
+  
+  }
+
+  // Fizicks
+  void FixedUpdate() {
+    float h = Input.GetAxis("Horizontal");
+    print(h);
+
+    if(h * this.GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+      this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+
+    if(Mathf.Abs(this.GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+      this.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(this.GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, this.GetComponent<Rigidbody2D>().velocity.y);
+  }
+}
