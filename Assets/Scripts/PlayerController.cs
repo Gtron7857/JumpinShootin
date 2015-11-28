ff094d88a6f8c1203304b66faff7cbbb8d3eddb6
Wade Shepardson <shepardson.wade@gmail.com>
2015-11-28 00:16:52 -0600

    camera follow, jumping, move decay, air slow drift, no player rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,54 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-  private float moveForce = 100f;
-  private float maxSpeed = 5f;
+  private float moveForce = 400f;
+  private float airMoveMod = .05f;
+  private float maxSpeed = 7f;
+  private float jumpForce = 700f;
+  private float velocityDecay = .8f;
+  private bool jump = false;
+  private bool grounded = false;
+  private Transform groundCheck;
+  private Rigidbody2D body;
 
   // Use this for initialization
   void Start() {
-  
+    groundCheck = transform.Find("Ground");
+    body = this.GetComponent<Rigidbody2D>();
   }
   
   // Update is called once per frame
   void Update() {
-  
+    grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+
+    if(Input.GetButtonDown("Jump") && grounded)
+      jump = true;
   }
 
   // Fizicks
   void FixedUpdate() {
     float h = Input.GetAxis("Horizontal");
+    float moveMod = 1f;
+    float maxMod = 1f;
 
-    if(h * this.GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
-      this.GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
+    if(!grounded)
+      moveMod *= airMoveMod;
 
-    if(Mathf.Abs(this.GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
-      this.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(this.GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, this.GetComponent<Rigidbody2D>().velocity.y);
+    if(h * body.velocity.x < maxSpeed)
+      body.AddForce(Vector2.right * h * moveForce * moveMod);
+
+    if(Mathf.Abs(body.velocity.x) > maxSpeed)
+      body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * maxSpeed, body.velocity.y);
+
+    if(grounded && h == 0) {
+      body.velocity = body.velocity * velocityDecay;
+    }
+
+    if(jump) {
+      body.AddForce(new Vector2(0f, jumpForce));
+      jump = false;
+    }
+
+    body.MoveRotation(0);
   }
 }
